e6ca4cdb6b9536e786981d70b7c3a42db438ec54
ritesh <riteshchaudhary375@gmail.com>
2024-07-24 12:24:15 +0545

    adding fonts from google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,9 @@ tailwind.config = {
     gridTemplateColumns: {
       auto: "repeat(auto-fit, minmax(200px, 1fr))",
     },
+    fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+      Ovo: ["Ovo", "serif"],
+    },
   },
 };
