1a1ba34851309676eb8ccdde25104e89b49ba9b8
ritesh <riteshchaudhary375@gmail.com>
2024-07-24 12:35:31 +0545

    adding extends features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 tailwind.config = {
   theme: {
-    gridTemplateColumns: {
-      auto: "repeat(auto-fit, minmax(200px, 1fr))",
-    },
-    fontFamily: {
-      Outfit: ["Outfit", "sans-serif"],
-      Ovo: ["Ovo", "serif"],
-    },
-    animation: {
-      spin_slow: "spin 6s linear infinite",
-    },
-    colors: {
-      lightHover: "#fcf4ff",
-      darkHover: "#2a004a",
+    extend: {
+      gridTemplateColumns: {
+        auto: "repeat(auto-fit, minmax(200px, 1fr))",
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+      animation: {
+        spin_slow: "spin 6s linear infinite",
+      },
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#2a004a",
+      },
     },
   },
 };
